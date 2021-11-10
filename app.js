13756ffce1a359dd527b6a9a8e5ef436f6d19472
Carlos Fung <carlos.fung@me.com>
2021-11-10 16:38:03 +0100

    Lab work

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -20,6 +22,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.all('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.all('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responsefromAPI => {
+      console.log('random beers:', responsefromAPI);
+      res.render('random-beer', { random: responsefromAPI });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
